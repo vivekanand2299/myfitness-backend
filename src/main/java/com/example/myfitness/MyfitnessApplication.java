0884361b99829d1330944f981d8a745b1c9a71f7
vivekanand2299 <technicalexpertise229@gmail.com>
2025-12-14 23:39:37 +0530

    Initial commit - MyFitness Android app

---
+++ src/main/java/com/example/myfitness/MyfitnessApplication.java
@@ -0,0 +1,14 @@
+package com.example.myfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+MyfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitnessApplication.class, args);
+	}
+
+}
